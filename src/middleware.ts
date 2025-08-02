d160d41173b4a2e933a34feab479cb7183c04841
Tan <lenhattan2313@gmail.com>
2025-08-02 17:22:49 +0700

    test middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,41 +1,8 @@
-import { NextResponse } from "next/server";
-import { auth } from "@/lib/auth";
+import { auth } from "./lib/auth";
 
-export default auth((req) => {
-  const { nextUrl } = req;
-  const pathname = nextUrl.pathname;
-  const isLoggedIn = !!req.auth;
-
-  console.log("isLoggedIn", isLoggedIn);
-
-  // Public routes (do not require auth)
-  const publicPaths = ["/signin", "/signout", "/offline"];
-  const isPublic = publicPaths.includes(pathname);
-
-  if (isPublic) {
-    // If already signed in and accessing auth page, redirect to home
-    if (isLoggedIn && (pathname === "/signin" || pathname === "/signout")) {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-    return NextResponse.next();
-  }
-
-  // Protected routes (require login)
-  if (!isLoggedIn) {
-    const signInUrl = new URL("/signin", req.url);
-    signInUrl.searchParams.set("callbackUrl", pathname);
-    return NextResponse.redirect(signInUrl);
-  }
-
-  return NextResponse.next();
-});
+export default auth;
 
 export const config = {
-  matcher: [
-    // Match all paths except:
-    // - API routes
-    // - _next static/image assets
-    // - metadata files
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
-  ],
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
