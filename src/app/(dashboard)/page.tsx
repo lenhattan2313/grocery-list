accac6d5251a56caa77cfc8a551c8b8b4a78be12
Tan <lenhattan2313@gmail.com>
2025-08-24 13:41:54 +0700

    Update dynamic import

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -2,11 +2,11 @@ import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { Suspense } from "react";
 
 import { getLists } from "@/app/actions/list";
-import { ListsPageClient } from "@/app/(dashboard)/list/list-page-client";
 import { PageSkeleton } from "@/components/common/page-skeleton";
 import { getQueryClient } from "@/lib/get-query-client";
 import { ShoppingListWithItems } from "@/types/list";
 import { PageHeaderWithSearch } from "@/components/common/page-header-with-search";
+import { ListsPageClient } from "@/components/dynamic-imports";
 
 export default async function ListsPage() {
   const queryClient = getQueryClient();
